8faf8bd257f3c281cddcc14a5255377284bd0b8a
Shogiiin <142786566+Shogiiin@users.noreply.github.com>
2024-02-29 16:22:10 +0100

    Reformed some code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 require('dotenv').config()
-
-//require('./register-commands')
-let commands = []
-commands = require('./register-commands')
-let commandList = ""
-for(c of commands) {
-    commandList += "\n" + c.name + " | " + c.description
-}
-
 const { Client, IntentsBitField, MessageCollector, EmbedBuilder, ActivityType } = require('discord.js')
+const eventhandler = require('./handlers/eventhandler')
+const getLocalCommands = require('./utils/getLocalCommands')
 
 const client = new Client({
     intents: [
@@ -19,35 +12,7 @@ const client = new Client({
     ]
 })
 
-client.once('ready', (c) => {
-    console.log(`\n${c.user.username} went online!`)
-    const Guilds = client.guilds.cache.map(guild => guild.id);
-    client.user.setActivity({
-        name: 'Just Chilling',
-        type: ActivityType.Custom,
-    })
-})
 
-client.on('interactionCreate', (interaction) => {
-    if(!interaction.isChatInputCommand()) return
-
-    switch(interaction.commandName) {
-        case 'help':
-            interaction.reply(`Here's a list of all my commands: ${commandList}`)
-        break;
-
-        case 'clear':
-            const numOfMessages = interaction.options.get('msg-number').value
-
-            const clearEmbed = new EmbedBuilder()
-                .setTitle(`Messages Cleared!`)
-                .setDescription(`I have cleared the last ${numOfMessages} messages in this channel.`)
-                .setColor(0xFAFA0F)
-
-
-            interaction.reply({ embeds: [clearEmbed] })
-        break;
-    }
-})
+eventhandler(client);
 
 client.login(process.env.TOKEN)
